de98f0b2e0546744162bf3717be0211bb349d1b0
Mamikon <marstamyan@mail.ru>
2024-01-18 00:01:28 +0400

    new pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,46 +7,50 @@
     menuBurger.classList.toggle("toggle")
     mobileMenu.classList.toggle("active")
   })
-})()
+})();
 
-// swiper run
+// swiper
+(function () {
+  const reviewsSwiper = new Swiper('.reviews-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    slidesPerView: 1,
+    loop: true, 
 
-// ;(function () {
-//   const swiper = new Swiper(".review-block", {
-//     loop: true,
-//     slidesPerView: 1,
-//     spaceBetween: 10,
-    
-//     navigation: {
-//         nextEl: '.review-arrow__right',
-//         prevEl: '.review-arrow__left',
-//     }    
-//   })
-// })()
-// var swiper = new Swiper(".mySwiper", {
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
 
+})();
 
+// modal
+(function () {
+  const showBtn = document.querySelector(".show-modal");
+  const maskModal = document.querySelector(".mask-modal");
+  const modal = document.querySelector(".modal");
+  const closeBtn = document.querySelector(".close-modal");
   
-window.onload = function () {
-    var reviewsSwiper = new Swiper('.reviews-swiper', {
-        slidesPerView: 1,
-        spaceBetween: 20,
-        slidesPerView: 1,
-        loop: true, 
-    
-        // autoplay: {
-        //     delay: 3000,
-        //     disableOnInteraction: true,
-        // },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-};
+  showBtn.addEventListener("click", () => {
+    maskModal.classList.add("active");
+    modal.classList.add("modal-active");
+  });
+  closeBtn.addEventListener("click", closeModal);
+  maskModal.addEventListener("click", closeModal);
+  
+  function closeModal() {
+    maskModal.classList.remove("active");
+    modal.classList.remove("modal-active");
+  }
+  
+  document.addEventListener("keyup", (e) => {
+    if (e.keyCode == 27) closeModal();
+  });
+  
 
+})();
